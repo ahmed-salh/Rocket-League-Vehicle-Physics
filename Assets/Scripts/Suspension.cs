41e43aa4cb956d1c1c43a3469c60fab162ac9c04
Ahmed Saleh <ahmedsaleh0002@gmail.com>
2024-09-04 02:36:09 +0300

    Basic vehicle controller
    Basic controls and suspension using Sphere cast

---
+++ Assets/Scripts/Suspension.cs
@@ -0,0 +1,140 @@
+﻿using UnityEngine;
+
+public class Suspension : MonoBehaviour
+{
+
+    #region Fields
+    public bool wheelFrontLeft;
+    public bool wheelFrontRight;
+    public bool wheelReerLeft;
+    public bool wheelReerRight;
+
+    private Rigidbody carRigidBody;
+    private float verticalInput;
+
+    [Header("Suspension")]
+    public float restLength;
+    public float springTravel;
+    public float springStiffness;
+    public float damperStiffness;
+
+    [Header("Friction")]
+    public float brakesFriction;
+    public float sideFriction;
+    private int brakeInput;
+
+    private float maxLength;
+    private float minLength;
+    private float lastLength;
+    private float springLength;
+    private float springVelocity;
+   
+    private float LongitudeForce;
+    private float LateralForce;
+    private Vector3 suspensionForce;
+    private Vector3 wheelVelocityLS;
+    private Vector3 localCarVelocity;
+
+    private float wheelAngle;
+    
+    [Header("Wheel")]
+    public Transform wheelRender;
+    public float wheelRadius;
+    public float steerAngle;
+    public float steerTime;
+    public float motorForce;
+    public float sidewayForce;
+    private float brakeForce;
+    #endregion
+
+    private void Awake() {
+
+        carRigidBody = transform.root.GetComponent<Rigidbody>();
+        minLength = restLength - springTravel;
+        maxLength = restLength + springTravel;
+    }
+
+    private void Update() {
+
+        verticalInput = Input.GetAxis("Vertical");
+        
+        if (Input.GetKey("space")) {
+
+            brakeInput = 1;
+           
+        } else {
+            
+            brakeInput = 0;
+        }
+
+        // Steer
+        wheelAngle = Mathf.Lerp(wheelAngle, steerAngle, steerTime * Time.deltaTime);
+        transform.localRotation = Quaternion.Euler(Vector3.up * wheelAngle);
+    }
+
+    private void FixedUpdate() {
+
+        localCarVelocity = transform.InverseTransformDirection(carRigidBody.velocity);
+
+        if (Physics.SphereCast(transform.position, wheelRadius, -transform.up, out RaycastHit hit, maxLength + wheelRadius)) {
+
+            SuspensionForce(hit);
+            Brakes();
+            ForwardFriction(hit);
+            //RollingResistance(hit);
+            //Drag();
+            LateralFriction(hit);
+
+            RotateWheelRender();
+        }
+    }
+
+    #region Custom Methods
+    private void ForwardFriction(RaycastHit hit) {
+
+        LongitudeForce = verticalInput * motorForce * (1 - brakeInput) - brakeForce;
+        carRigidBody.AddForceAtPosition(LongitudeForce * transform.forward, hit.point);
+
+    }
+
+    private void RollingResistance(RaycastHit hit) { 
+    
+    }
+
+    private void LateralFriction(RaycastHit hit) {
+
+        wheelVelocityLS = transform.InverseTransformDirection(carRigidBody.GetPointVelocity(hit.point));
+        LateralForce = wheelVelocityLS.x * sidewayForce;
+        carRigidBody.AddForceAtPosition(LateralForce * -transform.right, hit.point);
+    }
+
+    private void SuspensionForce(RaycastHit hit) {
+
+        lastLength = springLength;
+        //springLength = hit.distance - wheelRadius;
+        springLength = hit.distance;
+        springLength = Mathf.Clamp(springLength, minLength, maxLength);
+        springVelocity = (lastLength - springLength) / Time.fixedDeltaTime;
+        suspensionForce = (springStiffness * (restLength - springLength) + damperStiffness * springVelocity) * transform.up;
+        carRigidBody.AddForceAtPosition(suspensionForce, hit.point);
+    }
+
+    private void Drag() { 
+    
+    }
+
+    private void Brakes() {
+
+        brakeForce = 0.25f * carRigidBody.mass * (brakesFriction * localCarVelocity.z / Time.fixedDeltaTime) * brakeInput;
+    }
+
+    private void RotateWheelRender() {
+
+        wheelRender.Rotate(Vector3.right * Mathf.Rad2Deg * (wheelVelocityLS.z / wheelRadius) * Time.deltaTime * (1 - brakeInput));
+        wheelRender.position = transform.position - transform.up * springLength;
+    }
+    #endregion
+}
+
+
+
---
+++ Assets/Scripts/VehicleController.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class VehicleController : MonoBehaviour
+{
+    
+    private float steerInput;
+
+    public Suspension[] springBase;
+
+    [Header("Specs")]
+    public float wheelBase;
+    public float rearTrack;
+    public float turnRadius;
+    public float maximumVelocity;
+    public Transform COG;
+
+    private Rigidbody carRigidBody;
+    private Vector3 carVelocity;
+    private Vector3 MaxVelocity;
+    private float ackermanAngeleLeft;
+    private float ackermanAngeleRight;
+
+    private void Awake() {
+
+        carRigidBody = GetComponent<Rigidbody>();
+        carRigidBody.centerOfMass = COG.localPosition;
+        
+    }
+
+    private void Update() {
+
+
+        steerInput = Input.GetAxis("Horizontal");
+
+        if (steerInput > 0)
+        {
+            // turning right
+            ackermanAngeleLeft = Mathf.Rad2Deg * Mathf.Atan(wheelBase / (turnRadius + (rearTrack / 2))) * steerInput;
+            ackermanAngeleRight = Mathf.Rad2Deg * Mathf.Atan(wheelBase / (turnRadius - (rearTrack / 2))) * steerInput;
+
+        }
+
+        else if (steerInput < 0)
+        {
+            // turning left
+            ackermanAngeleLeft = Mathf.Rad2Deg * Mathf.Atan(wheelBase / (turnRadius - (rearTrack / 2))) * steerInput;
+            ackermanAngeleRight = Mathf.Rad2Deg * Mathf.Atan(wheelBase / (turnRadius + (rearTrack / 2))) * steerInput;
+
+        } else {
+
+            ackermanAngeleLeft = 0;
+            ackermanAngeleRight = 0;
+        }
+
+        foreach (Suspension wheel in springBase) {
+
+            if (wheel.wheelFrontLeft)
+                wheel.steerAngle = ackermanAngeleLeft;
+
+            if (wheel.wheelFrontRight)
+                wheel.steerAngle = ackermanAngeleRight;
+        }
+
+    }
+
+    private void FixedUpdate() {
+
+        carVelocity = carRigidBody.velocity;
+
+        // Set the maximum velocity for the car.
+        if (carVelocity.sqrMagnitude >= Mathf.Pow(maximumVelocity, 2)) {
+
+            MaxVelocity = carVelocity.normalized * maximumVelocity;
+            carRigidBody.velocity = MaxVelocity;
+        }
+        
+    }
+}
